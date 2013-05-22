284445545d136d2d52c3790f6840d5d3416ea729
Mathias Fussenegger <f.mathias@zignar.net>
2013-05-22 12:00:29 +0200

    change plugin registration

--- src/main/java/com/lovelysystems/plugin/facet/FacetPlugin.java
+++ src/main/java/com/lovelysystems/plugin/facet/FacetPlugin.java
@@ -1,19 +1,11 @@
 package com.lovelysystems.plugin.facet;
 
 import com.lovelysystems.facet.distinct.DistinctDateHistogramFacetParser;
-import com.lovelysystems.facet.distinct.InternalDistinctDateHistogramFacet;
-import com.lovelysystems.facet.latest.InternalLatestFacet;
-import com.lovelysystems.facet.latest.LatestFacetProcessor;
-import com.lovelysystems.facet.uncached.datehistogram.DateHistogramFacetProcessor;
-import com.lovelysystems.facet.uncached.datehistogram.InternalFullDateHistogramFacet;
-import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.plugins.AbstractPlugin;
 import org.elasticsearch.search.facet.FacetModule;
 
-/**
- * @author jukart
- */
+
 public class FacetPlugin extends AbstractPlugin {
 
     public FacetPlugin(Settings settings) {
@@ -29,16 +21,19 @@ public class FacetPlugin extends AbstractPlugin {
         return "Lovely Systems Plugins";
     }
 
-    @Override
-    public void processModule(Module module) {
-        if (module instanceof FacetModule) {
-            ((FacetModule) module).addFacetProcessor(DateHistogramFacetProcessor.class);
-            InternalFullDateHistogramFacet.registerStreams();
-            ((FacetModule) module).addFacetProcessor(DistinctDateHistogramFacetParser.class);
-            InternalDistinctDateHistogramFacet.registerStreams();
-            ((FacetModule) module).addFacetProcessor(LatestFacetProcessor.class);
-            InternalLatestFacet.registerStreams();
-
-        }
+    public void onModule(FacetModule module) {
+        module.addFacetProcessor(DistinctDateHistogramFacetParser.class);
     }
+
+    //@Override
+    //public void processModule(Module module) {
+    //    if (module instanceof FacetModule) {
+    //        ((FacetModule) module).addFacetProcessor(DateHistogramFacetProcessor.class);
+    //        InternalFullDateHistogramFacet.registerStreams();
+    //        ((FacetModule) module).addFacetProcessor(DistinctDateHistogramFacetParser.class);
+    //        InternalDistinctDateHistogramFacet.registerStreams();
+    //        ((FacetModule) module).addFacetProcessor(LatestFacetProcessor.class);
+    //        InternalLatestFacet.registerStreams();
+    //    }
+    //}
 }
