7c49c77b9d18533a99424c70aa779e2a0471c121
Mathias Fussenegger <f.mathias@zignar.net>
2013-07-26 10:20:35 +0200

    fixed a NullPointerException in client only mode

--- src/main/java/crate/elasticsearch/plugin/facet/FacetPlugin.java
+++ src/main/java/crate/elasticsearch/plugin/facet/FacetPlugin.java
@@ -1,10 +1,15 @@
 package crate.elasticsearch.plugin.facet;
 
 import crate.elasticsearch.facet.distinct.DistinctDateHistogramFacetParser;
+import crate.elasticsearch.facet.distinct.InternalDistinctDateHistogramFacet;
 import crate.elasticsearch.facet.latest.LatestFacetParser;
+import org.elasticsearch.common.collect.Lists;
+import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.plugins.AbstractPlugin;
 import org.elasticsearch.search.facet.FacetModule;
+
+import java.util.Collection;
 
 
 public class FacetPlugin extends AbstractPlugin {
@@ -20,6 +25,13 @@ public class FacetPlugin extends AbstractPlugin {
     @Override
     public String description() {
         return "Time-Facets Plugins";
+    }
+
+    @Override
+    public Collection<Class<? extends Module>> modules() {
+        Collection<Class<? extends Module>> modules = Lists.newArrayList();
+        modules.add(TimeFacetsModule.class);
+        return modules;
     }
 
     public void onModule(FacetModule module) {
